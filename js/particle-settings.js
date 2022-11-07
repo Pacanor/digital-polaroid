99ecb861d19c49cf9f60c0004a3255e2e8b09376
Papias Banda <papiasb@gmail.com>
2022-11-07 11:36:21 +0200

    Feature: updated placeholder screen

--- js/particle-settings.js
+++ js/particle-settings.js
@@ -1,10 +1,10 @@
 particlesJS("particles-js", {
     particles: {
         number: { value: 160, density: { enable: true, value_area: 800 } },
-        color: { value: "#ffffff" },
+        color: { value: "#41a1b1" },
         shape: {
         type: "circle",
-        stroke: { width: 0, color: "#000000" },
+        stroke: { width: 0, color: "#41a1b1" },
         polygon: { nb_sides: 5 },
         image: { src: "img/github.svg", width: 100, height: 100 }
         },
@@ -21,7 +21,7 @@ particlesJS("particles-js", {
         line_linked: {
         enable: false,
         distance: 150,
-        color: "#ffffff",
+        color: "#41a1b1",
         opacity: 0.4,
         width: 1
         },
