39ff0fdd5eefa4553298ea2d806006626fba0008
Papias Banda <papiasb@gmail.com>
2022-11-19 09:59:21 +0200

    Fix: extended deadline

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@
     const cfg = {
 
         // Countdown Timer Final Date
-        finalDate : 'November 18, 2022 08:00:00',
+        finalDate : 'November 25, 2022 08:00:00',
         // MailChimp URL
         mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
 
